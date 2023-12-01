2e338e1ce7aeeb31f724cd433ebd58ea873410fd
HumbleBird <wltnsdid67@naver.com>
2023-12-01 20:37:42 +0900

    Initial commit

---
+++ DummyClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace DummyClient
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
