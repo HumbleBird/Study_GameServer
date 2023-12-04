4959ac557ac54cac96e97f193b6154705ae544e3
HumbleBird <wltnsdid67@naver.com>
2023-12-04 12:56:03 +0900

    컴파일러 최적화

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,59 +6,37 @@ namespace ServerCore
 {
     class Program
     {
-        static void MainThread(object state)
+        volatile static bool _stop = false;
+
+        static void ThreadMain()
         {
-            for (int i = 0; i < 5; i++)
+            Console.WriteLine("쓰레드 시작!");
+
+            while (_stop == false)
             {
-                Console.WriteLine("Hello Thread!");
+
             }
+
+            Console.WriteLine("쓰레드 종료!");
+
         }
 
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(5, 5);
+            Task t = new Task(ThreadMain);
+            t.Start();
 
-            for (int i = 0; i < 5; i++)
-            {
-                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
-                t.Start();
-            }
+            Thread.Sleep(1000);
 
-            //
+            _stop = true;
 
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중 ");
 
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    ThreadPool.QueueUserWorkItem((obj) =>
-            //    {
-            //        while (true)
-            //        {
+            t.Wait();
 
-            //        }
-            //    });
+            Console.WriteLine("종료 성공");
 
-            //}
-            ThreadPool.QueueUserWorkItem(MainThread);
-
-            //for (int i = 0; i < 1000; i++)
-            //{
-            //    Thread t = new Thread(MainThread);
-            //    //t.Name = "Test Thread";
-            //    t.IsBackground = true;
-            //    t.Start();
-            //}
-
-
-            //Console.WriteLine("Wating for Thread");
-            //t.Join();
-
-            //Console.WriteLine("Hello World");
-
-            while (true)
-            {
-
-            }
         }
     }
 }
