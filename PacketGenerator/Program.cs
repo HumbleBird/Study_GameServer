cd6063d9f891a707c4e32909c5edc01fd4f7da77
HumbleBird <wltnsdid67@naver.com>
2023-12-07 21:51:11 +0900

    Packet Generator #1

--- PacketGenerator/Program.cs
+++ PacketGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 
 namespace PacketGenerator
 {
@@ -6,7 +7,84 @@ namespace PacketGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            XmlReaderSettings settings = new XmlReaderSettings()
+            {
+                IgnoreComments = true,
+                IgnoreWhitespace = true
+            };
+
+            using( XmlReader r = XmlReader.Create("PDL.xml", settings))
+            {
+                r.MoveToContent();
+
+                while (r.Read())
+                {
+                    if (r.Depth == 1 && r.NodeType == XmlNodeType.Element)
+                        ParsePacket(r);
+
+                    //Console.WriteLine(r.Name + " " + r["name"]);
+                }
+            }
+
+        }
+
+        public static void ParsePacket(XmlReader r)
+        {
+            if (r.NodeType == XmlNodeType.EndElement)
+                return;
+
+            if (r.Name.ToLower() != "packet")
+            {
+                Console.WriteLine("Invalid packet node");
+                return;
+            }
+
+            string packetName = r["name"];
+            if (string.IsNullOrEmpty(packetName))
+            {
+                Console.WriteLine("Packet Without Name");
+                return;
+            }
+
+            ParseMembers(r);
+        }
+
+        public static void ParseMembers(XmlReader r)
+        {
+            string packetName = r["name"];
+
+            int depth = r.Depth + 1;
+            while (r.Read())
+            {
+                if (r.Depth != depth)
+                    break;
+
+                string memberName = r["name"];
+                if(string.IsNullOrEmpty(memberName))
+                {
+                    Console.WriteLine("Member witout name");
+                    return;
+                }
+
+                string memberType = r.Name.ToLower();
+                switch (memberType)
+                {
+                    case "bool":
+                    case "byte":
+                    case "short":
+                    case "ushort":
+                    case "int":
+                    case "long":
+                    case "float":
+                    case "double":
+                    case "string":
+                    case "list":
+                        break;
+                    default:
+                        break;
+                }
+
+            }
         }
     }
 }
