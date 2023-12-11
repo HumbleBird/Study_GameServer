eb943a45f1ef5f264ca2e69c7541168c222c2942
HumbleBird <wltnsdid67@naver.com>
2023-12-11 11:19:54 +0900

    JobQueue #2

--- Server/Session/ClientSession.cs
+++ Server/Session/ClientSession.cs
@@ -41,7 +41,7 @@ namespace Server
 
         public override void OnSend(int numOfBytes)
         {
-            Console.WriteLine($"Transferrd bytes: {numOfBytes}");
+            //Console.WriteLine($"Transferrd bytes: {numOfBytes}");
         }
     }
 }
