ad8275702edf3e91a962ecb83000c2db62cbd150
HumbleBird <wltnsdid67@naver.com>
2023-12-11 15:13:54 +0900

    유니티 연동 #2

--- Common/Packet/GenPackets.bat
+++ Common/Packet/GenPackets.bat
@@ -3,3 +3,8 @@ XCOPY /Y GenPackets.cs "../../DummyClient/Packet"
 XCOPY /Y GenPackets.cs "../../Server/Packet"
 XCOPY /Y ClientPacketManager.cs "../../DummyClient/Packet"
 XCOPY /Y ServerPacketManager.cs "../../Server/Packet"
+
+
+
+XCOPY /Y ClientPacketManager.cs "D:/LeeTaeSeop/Programing/Game Engine/Unity/PortPolio/Sutdy/Assets/005_Rookiss_GameServer/Packet"
+XCOPY /Y GenPackets.cs "D:/LeeTaeSeop/Programing/Game Engine/Unity/PortPolio/Sutdy/Assets/005_Rookiss_GameServer/Packet"
