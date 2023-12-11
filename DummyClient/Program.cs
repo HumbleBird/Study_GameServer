2348565fe17aaeb2385f184398b9f089dc4ef3f0
HumbleBird <wltnsdid67@naver.com>
2023-12-11 14:25:31 +0900

    유니티 연동 #1

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -24,7 +24,7 @@ namespace DummyClient
 
             connector.Connect(endPoint, 
                 () => { return SessionManager.Instance.Generate(); },
-                500);
+                10);
 
             while (true)
             {
